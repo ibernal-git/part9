822f4418025111abe2ffe450d6e4fe6c0aeeb8dd
Imanol Bernal <ibernal@icloud.com>
2021-04-25 12:52:31 +0200

    Complete Exercise 9.17

---
+++ patientor/src/PatientInfo/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from "../constants";
+import { Header, List, Icon } from "semantic-ui-react";
+
+import { Patient } from '../types';
+
+import { useStateValue } from "../state";
+
+const PatientInfo = () => {
+
+  const { id } = useParams<{ id: string }>();
+  const [{ patients }, dispatch] = useStateValue();
+
+  useEffect(() => {
+
+    const getPatient = async () => {
+
+      if (!patients[id] || !patients[id].ssn) {
+        try {
+          const patient = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+          console.log(patient);
+          if (!patient) {
+            return (<div>no patient found</div>);
+          }
+          dispatch({ type: "GET_PATIENT", payload: patient.data });
+        } catch (e) {
+          const error = e as Error;
+          return (<div>{`error: ${error.message}`}</div>);
+        }
+      }
+    };
+    void getPatient();
+  }, [dispatch]);
+
+  if (!patients[id]) {
+    return (
+      <div>error</div>
+    );
+  }
+  // mars
+
+  return (
+    <div>
+      <Header as="h2">{patients[id].name}
+      {
+        patients[id].gender === 'other' 
+        ? <Icon name='genderless' /> 
+        : patients[id].gender === 'female' 
+          ? <Icon name='venus' /> 
+          : <Icon name='mars' />
+      }
+      </Header>
+      <List>
+        <List.Item>ssn: {patients[id].ssn}</List.Item>
+        <List.Item>occupation: {patients[id].occupation}</List.Item>
+        <List.Item>gender: {patients[id].gender}</List.Item>
+      </List>
+    </div>
+  );
+};
+
+export default PatientInfo;
